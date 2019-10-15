88c7a4780633c681e0ebb3f6c6674c7550d7de90
Vectormike <victorjonah199@gmail.com>
2019-10-15 22:04:29 +0100

    Addd multer library to route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,39 @@
-const express=require ('express');
-const loginUser = require('../controller/Login');
+const express = require("express");
+const loginUser = require("../controller/login");
 const router = express.Router();
+const multer = require("multer");
 
-router.post('/login', loginUser);
+// Start of Multer //
+// Multer
 
+// Storage fro Multer
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + file.originalname);
+  }
+});
 
-module.exports=router;
+const upload = multer({ dest: "uploads/" });
+const fileUpload = upload.single("files");
+// End of Multer //
+
+router.post("/login", loginUser);
+
+// Test for Multer
+router.post("/upload", fileUpload, (req, res) => {
+  //console.log(req.file);
+  if (req.file.path) {
+    res.json({
+      message: req.file
+    });
+    return;
+  }
+  res.json({
+    error: "Not uploading"
+  });
+});
+
+module.exports = router;
