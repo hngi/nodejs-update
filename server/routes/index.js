13307a9d394dcf5c79692fb552786e16476d2360
Vectormike <victorjonah199@gmail.com>
2019-10-16 00:08:53 +0100

    Removed Multer from route.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,28 +1,11 @@
 const express = require("express");
 const loginUser = require("../controller/login");
 const router = express.Router();
-const multer = require("multer");
 
-// Start of Multer //
-
-// Storage for Multer
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + file.originalname);
-  }
-});
-
-const upload = multer({ storage: storage });
-const fileUpload = upload.single("files");
-
-// End of Multer //
+const fileUpload = require("../middleware/uploads");
 
 router.post("/login", loginUser);
 
-// Test for Multer
 router.post("/upload", fileUpload, (req, res) => {
   //console.log(req.file);
   if (req.file.path) {
