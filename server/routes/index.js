75e39b92df23e1686809769f7abaa4997d4d3462
Iyara Ferguson <fergusoniyara@gmail.com>
2019-10-15 16:29:18 +0100

    Create Middleware to check user input before login

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 const express=require ('express');
-
+const validateNewUser = require('../middlewares/newuser');
 const router = express.Router();
 
 // router.use('/auth', auth);
+
+router.post('/login', validateNewUser);
 
 
 module.exports=router;
