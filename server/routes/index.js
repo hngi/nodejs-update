2e6c0a552170e6c8beb27013a8ecbd2bc590ad79
Vectormike <victorjonah199@gmail.com>
2019-10-16 04:36:29 +0100

    resolved conflict

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const loginUser = require("../controller/login");
 const router = express.Router();
 
+const loginUser = require("../controller/login");
+const registerUser = require("../controller/register");
 const fileUpload = require("../middleware/uploads");
 
 router.post("/login", loginUser);
+router.post("/signup", registerUser);
 
 router.post("/upload", fileUpload, (req, res) => {
   //console.log(req.file);
