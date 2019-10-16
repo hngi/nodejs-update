40c348e133220f6b87221a11c3f04841b155ca51
cavdy <cavdyofficials@gmail.com>
2019-10-16 15:46:45 +0100

    shorten url

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,16 @@
-const express = require("express");
+const express = require('express');
+const { redirectShortenUrl } = require('../controller/shortUrl');
+const { findShortenUrl } = require('../middleware/findShortenUrl');
 const router = express.Router();
 
-const loginUser = require("../controller/Login");
-const registerUser = require("../controller/Register");
+const loginUser = require('../controller/login');
+const registerUser = require('../controller/register');
 
-router.post("/login", loginUser);
-router.post("/register", registerUser);
+const shortUrl = require('./shortUrl');
+
+router.get('/:shortenId', findShortenUrl, redirectShortenUrl);
+router.post('/api/auth/login', loginUser);
+router.post('/api/auth/register', registerUser);
+router.use('/api', shortUrl);
 
 module.exports = router;
