9f2088c41307d5e62ccf2677a6e011ffb674491c
Vectormike <victorjonah199@gmail.com>
2019-10-16 04:42:10 +0100

    ...

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,22 +3,8 @@ const router = express.Router();
 
 const loginUser = require("../controller/login");
 const registerUser = require("../controller/register");
-const fileUpload = require("../middleware/uploads");
 
 router.post("/login", loginUser);
 router.post("/signup", registerUser);
-
-router.post("/upload", fileUpload, (req, res) => {
-  //console.log(req.file);
-  if (req.file.path) {
-    res.json({
-      message: req.file
-    });
-    return;
-  }
-  res.json({
-    error: "Not uploading"
-  });
-});
 
 module.exports = router;
