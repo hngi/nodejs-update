401c2bd3917d8fe50abdc3b55649862eece22aa9
unknown <fegoworks@hotmail.com>
2019-10-16 10:29:48 +0100

    bug(fix):fix password hash

--- server/controller/register.js
+++ server/controller/register.js
@@ -1,41 +1,58 @@
-
-const {extractErrors} = require('../helpers/helper');
+const {
+    extractErrors
+} = require('../helpers/helper');
 const mongoose = require('mongoose');
-const {User} = require('../models/User');
-const {validate} = require('../models/User')
-const jwt = require('jsonwebtoken'); 
+const {
+    User
+} = require('../models/User');
+const {
+    validate
+} = require('../models/User');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
 const register = async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).json({
-            status:"error",
-            error:error.details[0].message
+    const {
+        error
+    } = validate(req.body);
+    if (error)
+        return res.json({
+            success: false,
+            message: error.details[0].message
         });
-    //checking if the user already exist 
-    let userhere = await User.findOne({ email: req.body.email });
-    if (userhere) return res.status(400).json({
-        status:"error",
-        error: "user already exist"
-    })
-    //creating user 
-    var user = new User({
-        username: req.body.username ,
-        email: req.body.email ,
-        password: req.body.password 
-    })
+    //checking if the user already exist
+    let userhere = await User.findOne({
+        email: req.body.email
+    });
+    if (userhere)
+        return res.json({
+            success: false,
+            message: 'User already exists'
+        });
+    //creating user
     const salt = await bcrypt.genSalt(10);
-    req.body.password = await bcrypt.hash(user.password, salt);
+    const hash = await bcrypt.hash(req.body.password, salt);
+
+    var user = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hash
+    });
+
     await user.save();
     //creating jwt
-    const token = jwt.sign({_id:user._id} , 'secret')
+    const token = jwt.sign({
+        _id: user._id
+    }, 'secret');
     res.header('x-auth-token', token).json({
-        _id:user._id,
-        username: user.username,
-        email : user.email,
-        token: token
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        },
+        token: token,
+        success: true
     });
-}
-
+};
 
 module.exports = register;
