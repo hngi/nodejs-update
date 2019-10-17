b2aaf2a8bfebc91be95c00e0059d6ffd06e24fd4
MacGee20 <giftomodu20@gmail.com>
2019-10-18 00:45:18 +0100

    _id field

--- server/models/ShortenLink.js
+++ server/models/ShortenLink.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 
 const urlShortenSchema = new Schema({
 
-  _id:mongoose.Schema.Types,ObjectId,
+  _id: mongoose.Schema.Types.ObjectId,
   cloudinaryUrl: String,
   shortUrlParam: String,
   shortUrl: String,
