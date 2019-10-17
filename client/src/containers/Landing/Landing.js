ab8fed71f9e42a4c55705896f63f2b6e7c97665e
Vectormike <victorjonah199@gmail.com>
2019-10-17 12:46:43 +0100

    Added dispatch

--- client/src/containers/Landing/Landing.js
+++ client/src/containers/Landing/Landing.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import './Landing.css';
-import GoogleAuth from '../../components/GoogleAuth/GoogleAuth';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { GoogleLogout } from 'react-google-login';
-import { logout } from '../../actions/auth';
-const Landing = ({ isSignedInWithGoogle, logout }) => {
+import React from "react";
+import "./Landing.css";
+import Share from "../../components/Share/share";
+import { showShare } from "../../actions/share";
+import GoogleAuth from "../../components/GoogleAuth/GoogleAuth";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { GoogleLogout } from "react-google-login";
+import { logout } from "../../actions/auth";
+
+const Landing = ({ isSignedInWithGoogle, logout, showShare, hide }) => {
   return (
     <div>
       <header>
         <nav>
-          <Link to='/'>XSHARE</Link>
-          <ul className='sub-link'>
-            {/* <li>About</li> */}
+          <Link to="/">XSHARE</Link>
+          <ul className="sub-link">
             {isSignedInWithGoogle ? (
               <li>
                 <GoogleLogout
-                  id='googleLogOutBtn'
-                  clientId='97829381082-8imeelchtkuvfcd47q0dgia1p0l91msr.apps.googleusercontent.com'
-                  buttonText='Logout'
+                  id="googleLogOutBtn"
+                  clientId="97829381082-8imeelchtkuvfcd47q0dgia1p0l91msr.apps.googleusercontent.com"
+                  buttonText="Logout"
                   onLogoutSuccess={logout}
-                  onFailure={() => logout()}
+                  onFailure={logout}
                 />
               </li>
             ) : (
@@ -31,109 +33,33 @@ const Landing = ({ isSignedInWithGoogle, logout }) => {
           </ul>
         </nav>
       </header>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-lg-6'>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">
             <h1>The most seamless file transfer experience ever.</h1>
+            <label htmlFor>Upload file</label>
+            <input className="form-control-file" type="file" />
+            <br />
             <button
-              id='btn-email'
-              className='btn btn-primary'
-              onClick={() => {
-                document
-                  .getElementsByClassName('form2')[0]
-                  .classList.add('d-none');
-                var form1 = document.getElementsByClassName('form1')[0];
-                form1.classList.contains('d-none')
-                  ? form1.classList.remove('d-none')
-                  : form1.classList.add('d-none');
-              }}>
-              Upload by email
+              type="submit"
+              className="btn btn-primary float-right"
+              value="Share"
+              onClick={showShare}
+            >
+              {" "}
+              Share
             </button>
-            <button
-              id='btn-link'
-              className='btn btn-primary ml-5'
-              onClick={() => {
-                document
-                  .getElementsByClassName('form1')[0]
-                  .classList.add('d-none');
-                var form2 = document.getElementsByClassName('form2')[0];
-                form2.classList.contains('d-none')
-                  ? form2.classList.remove('d-none')
-                  : form2.classList.add('d-none');
-              }}>
-              Upload by link
+            <button id="back" className="btn btn-primary float-left">
+              Cancel
             </button>
-            {''}
-            <div className='form1 d-none'>
-              <form action>
-                <div className='form-group'>
-                  <label htmlFor='email'>Email</label>
-                  <input
-                    className='form-control'
-                    type='email'
-                    placeholder='Your email address'
-                    id='email'
-                  />
-                  <label htmlFor='Remail'>Reciever Email</label>
-                  <input
-                    className='form-control'
-                    type='email'
-                    placeholder='email address of Reciever'
-                    id='Remail'
-                  />
-                  <label htmlFor='email'>Message</label>
-                  <textarea
-                    className='form-control'
-                    name
-                    id
-                    cols={10}
-                    rows={3}
-                    defaultValue={''}
-                  />
-                  <label htmlFor>Upload file</label>
-                  <input className='form-control-file' type='file' />
-                  <input
-                    type='submit'
-                    className='btn btn-primary float-right'
-                    defaultValue='Transfer'
-                  />
-                  <button id='back' className='btn btn-primary float-left'>
-                    Cancel
-                  </button>
-                </div>
-              </form>
-            </div>
-            <div className='form2 d-none'>
-              <form action>
-                <div className='form-group'>
-                  <label htmlFor='email'>Message</label>
-                  <textarea
-                    className='form-control'
-                    name
-                    id
-                    cols={10}
-                    rows={3}
-                    defaultValue={''}
-                  />
-                  <label htmlFor>Upload file</label>
-                  <input className='form-control-file' type='file' />
-                  <input
-                    type='submit'
-                    className='btn btn-primary float-right'
-                    defaultValue='Generate Link'
-                  />
-                  <button id='back' className='btn btn-primary float-left'>
-                    Cancel
-                  </button>
-                </div>
-              </form>
-            </div>
+
+            {hide ? null : <Share />}
           </div>
-          <div className='col-lg-6'>
+          <div className="col-lg-6">
             <div>
               <img
-                src='https://res.cloudinary.com/dvbwpicno/image/upload/v1571178848/yg8ch6bhftwzooxugbuo.png'
-                alt='cloudimage'
+                src="https://res.cloudinary.com/dvbwpicno/image/upload/v1571178848/yg8ch6bhftwzooxugbuo.png"
+                alt="cloudimage"
               />
             </div>
           </div>
@@ -143,9 +69,16 @@ const Landing = ({ isSignedInWithGoogle, logout }) => {
   );
 };
 const mapStateToProps = state => ({
-  isSignedInWithGoogle: state.auth.isSignedInWithGoogle
+  isSignedInWithGoogle: state.auth.isSignedInWithGoogle,
+  hide: state.showShare.hide
 });
+
+const mapDispatchToProps = dispatch => ({
+  showShare: () => dispatch(showShare()),
+  logout: () => dispatch(logout())
+});
+
 export default connect(
   mapStateToProps,
-  { logout }
+  mapDispatchToProps
 )(Landing);
