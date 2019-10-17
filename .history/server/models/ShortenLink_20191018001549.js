73c24a96565f1d03f39dbc75db98749e2282b655
MacGee20 <giftomodu20@gmail.com>
2019-10-18 00:20:03 +0100

    unique id field

--- .history/server/models/ShortenLink_20191018001549.js
+++ .history/server/models/ShortenLink_20191018001549.js
@@ -3,14 +3,11 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const urlShortenSchema = new Schema({
-
-  _id:mongoose.Schema.Types,ObjectId,
   cloudinaryUrl: String,
   shortUrlParam: String,
   shortUrl: String,
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
-
 });
 
 const shortenLink = mongoose.model('ShortLink', urlShortenSchema);
