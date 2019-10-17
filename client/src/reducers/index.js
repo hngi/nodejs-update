e9523d2bd182167ff1e4b1707adfd44333e6eb35
Vectormike <victorjonah199@gmail.com>
2019-10-17 12:53:07 +0100

    Added showShare reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,8 @@
-import { combineReducers } from 'redux';
-import auth from './auth';
-import alert from './alert';
+import { combineReducers } from "redux";
+import auth from "./auth";
+import alert from "./alert";
+import showShare from "./share";
 
-const rootReducer = combineReducers({ auth,alert });
+const rootReducer = combineReducers({ auth, alert, showShare });
 
 export default rootReducer;
