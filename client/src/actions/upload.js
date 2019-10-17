7d3b71bd20cbdad4c1cae9257cb86c1246f8974f
Busola <olowubusolaolutayo1992@gmail.com>
2019-10-17 15:52:53 +0100

    react implementation

--- client/src/actions/upload.js
+++ client/src/actions/upload.js
@@ -12,10 +12,11 @@ import axios from 'axios';
 // const base_url = '';
 
 const base_url = 'http://localhost:4000';
-export const upload = file => async dispatch => {
+export const upload = (name,to,file,link,isEmail) => async dispatch => {
   const body = JSON.stringify({
-    file
+    name,to,file,link,isEmail
   });
+  console.log(body)
   const config = {
     headers: {
       'Content-Type': 'application/json',
@@ -35,91 +36,11 @@ export const upload = file => async dispatch => {
         payload: response.data
       });
       dispatch(setAlert(response.data.message, 'success'));
-    } else {
+    } else if(response.data.message == 'Link shortened successfully') {
       dispatch(setAlert(response.data.message, 'danger'));
-      dispatch({
-        type: UPLOAD_FILE_FAIL,
-        payload: response.data.message
-      });
-    }
-  } catch (error) {
-    dispatch(setAlert(error.toString(), 'danger'));
-
-    dispatch({
-      type: UPLOAD_FILE_FAIL,
-      payload: error.toString()
-    });
-  }
-};
-export const sendEmail = (to, link) => async dispatch => {
-  const body = JSON.stringify({
-    to,
-    link
-  });
-  const config = {
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    }
-  };
-  try {
-    const response = await axios.post(
-      base_url + '/api/auth/sendEmail',
-      body,
-      config
-    );
-    console.log(response);
-    if (response.data.success) {
-      dispatch({
-        type: SEND_EMAIL_SUCCESS,
-        payload: response.data
-      });
-      dispatch(setAlert(response.data.message, 'success'));
-    } else {
+    }else if(response.data.message.includes('File successfully sent to')){
       dispatch(setAlert(response.data.message, 'danger'));
-      dispatch({
-        type: SEND_EMAIL_FAIL,
-        payload: response.data.message
-      });
-    }
-  } catch (error) {
-    dispatch(setAlert(error.toString(), 'danger'));
-
-    dispatch({
-      type: SEND_EMAIL_FAIL,
-      payload: error.toString()
-    });
-  }
-};
-export const copyLink = link => async dispatch => {
-  const body = JSON.stringify({
-    file
-  });
-  const config = {
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    }
-  };
-  try {
-    const response = await axios.post(
-      base_url + '/api/auth/upload',
-      body,
-      config
-    );
-    console.log(response);
-    if (response.data.success) {
-      dispatch({
-        type: UPLOAD_FILE_SUCCESS,
-        payload: response.data
-      });
-      dispatch(
-        setAlert(
-          'File upload was successful,please choose how you want to send your file',
-          'success'
-        )
-      );
-    } else {
+    }else{
       dispatch(setAlert(response.data.message, 'danger'));
       dispatch({
         type: UPLOAD_FILE_FAIL,
