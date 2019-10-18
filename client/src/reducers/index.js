a198b1e05204d0ca36b710e45f803e68e7db7653
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-10-18 14:51:34 +0000

    backend files

--- client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import auth from './auth';
-import alert from './alert';
-import upload from './upload';
-
-const rootReducer = combineReducers({ auth,alert,upload });
-
-export default rootReducer;
