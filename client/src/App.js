5f3adf4df5c818f50ead0afd6e8aca8ca1561433
Vectormike <victorjonah199@gmail.com>
2019-10-19 19:15:47 +0100

    ...

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store';
-import Alert from './components/Alert/Alert';
-import Landing from 'containers/Landing/Landing';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import Alert from "./components/Alert/Alert";
+import Landing from "./containers/Landing/Landing";
 
 const App = () => {
   return (
@@ -11,7 +11,7 @@ const App = () => {
       <Router>
         <Alert />
         <Switch>
-          <Route path='/' component={Landing} />
+          <Route path="/" component={Landing} />
         </Switch>
       </Router>
     </Provider>
