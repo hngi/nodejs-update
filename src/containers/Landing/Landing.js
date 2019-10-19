2fd6359897e32c834f12db8adf5527b78c92c6fb
Busola <olowubusolaolutayo1992@gmail.com>
2019-10-19 12:07:21 +0100

    update Landing.js

--- src/containers/Landing/Landing.js
+++ src/containers/Landing/Landing.js
@@ -39,24 +39,23 @@ const shortUrl=uploadstate.shortUrl
           <Link className='header' to='/'>
             XSHARE
           </Link>
-//           <ul className='sub-link'>
-//             {/* <li>About</li> */}
-//             {isSignedInWithGoogle ? (
-//               <li>
-//                 <GoogleLogout
-//                   id='googleLogOutBtn'
-//                   clientId='97829381082-8imeelchtkuvfcd47q0dgia1p0l91msr.apps.googleusercontent.com'
-//                   buttonText='Logout'
-//                   onLogoutSuccess={logout}
-//                   onFailure={() => logout()}
-//                 />
-//               </li>
-//             ) : (
-//               <li className='login'>
-//                 <GoogleAuth />
-//               </li>
-//             )}
-//           </ul>
+           {/* <ul className='sub-link'>
+             {isSignedInWithGoogle ? (
+               <li>
+                 <GoogleLogout
+                   id='googleLogOutBtn'
+                   clientId='97829381082-8imeelchtkuvfcd47q0dgia1p0l91msr.apps.googleusercontent.com'
+                   buttonText='Logout'
+                   onLogoutSuccess={logout}
+                   onFailure={() => logout()}
+                 />
+               </li>
+             ) : (
+               <li className='login'>
+                 <GoogleAuth />
+               </li>
+             )}
+           </ul> */}
         </nav>
       </header>
 
