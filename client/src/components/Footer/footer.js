ad42b04f1d3e939cdf583d0ace4bcd72867b5be5
Busola <olowubusolaolutayo1992@gmail.com>
2019-10-20 07:46:07 +0100

    add privacy page

--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -1,17 +1,17 @@
-import React from "react";
-import { MDBContainer, MDBFooter } from "mdbreact";
+import React from 'react';
+import { MDBContainer, MDBFooter } from 'mdbreact';
 
-const FooterPage = () => {
+const Footer = () => {
   return (
-    <MDBFooter color="blue" className="font-small pt-4 mt-4">
-      <div className="footer-copyright text-center py-3">
+    <MDBFooter color='blue' className='font-small pt-4 mt-4'>
+      <div className='footer-copyright text-center py-3'>
         <MDBContainer fluid>
-          &copy; {new Date().getFullYear()} Copyright:{" "}
-          <a href="https://www.hng.tech"> Built by HNG 6.0 Interns </a>
+          &copy; {new Date().getFullYear()} Copyright:{' '}
+          <a href='https://www.hng.tech'> Built by HNG 6.0 Interns </a>
         </MDBContainer>
       </div>
     </MDBFooter>
   );
 };
 
-export default FooterPage;
+export default Footer;
