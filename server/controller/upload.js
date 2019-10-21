4ff557d55704abdb5325b82d75c5efcbc6401a67
Vectormike <victorjonah199@gmail.com>
2019-10-21 12:22:58 +0100

    ...

--- server/controller/upload.js
+++ server/controller/upload.js
@@ -1,6 +1,6 @@
-const multerHelper = require('../middleware/multer');
+const multerHelper = require("../middleware/multer");
 const dataUri = multerHelper.dataUri;
-const { uploader, cloudinaryConfig } = require('../config/cloudinary');
+const { uploader, cloudinaryConfig } = require("../config/cloudinary");
 const upload = (req, res, next) => {
   if (req.file) {
     const file = dataUri(req).content;
@@ -8,12 +8,12 @@ const upload = (req, res, next) => {
       .upload(file)
       .then(result => {
         const fileUploadedUrl = result.url;
-        res.locals['cloudinaryUrl'] = fileUploadedUrl;
+        res.locals["cloudinaryUrl"] = fileUploadedUrl;
         next();
       })
       .catch(err => {
         res.json({
-          message: 'Something went wrong while processing your request',
+          message: "Something went wrong while processing your request",
           success: false,
           data: {
             err
