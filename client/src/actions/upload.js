e949b8d03018ee2e1780315eb900248dc6f40520
Busola <olowubusolaolutayo1992@gmail.com>
2019-10-22 22:46:54 +0100

    check for file type and size and other edge cases

--- client/src/actions/upload.js
+++ client/src/actions/upload.js
@@ -5,7 +5,7 @@ import {
   SEND_EMAIL_FAIL,
   DOWNLOAD_LINK_FAIL,
   DOWNLOAD_LINK_SUCCESS,
-  HIDE_LINK,LOADING
+  LOADING
 } from './types';
 import { setAlert } from './alert';
 
@@ -14,116 +14,94 @@ import axios from 'axios';
 
 const base_url = 'https://x-shareserver.herokuapp.com';
 export const hidelink = () => async => dispatch => {
-  dispatch({
-    type: HIDE_LINK
-  });
-};
-export const uploadFile = file => async dispatch => {
-  const fd = new FormData();
-  
-  // fd.append('name', name);
-  // fd.append('to', to);
-  // fd.append('isEmail', true);
-  fd.append('file', file);
-  
-  dispatch({
-    type: LOADING
-  });
-  const config = {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'multipart/form-data'
-    }
-  };
-  try {
-    const response = await axios.post(
-      base_url + '/api/auth/upload',
-      fd,
-      config
-    );
-    console.log(response);
-    if (response.data.success) {
-      dispatch({
-        type: UPLOAD_FILE_SUCCESS,
-        payload: response.data
-      });
-      dispatch(setAlert(response.data.message, 'success'));
-    } 
-    else {
-      dispatch(setAlert('Error uploading file', 'danger'));
-      dispatch({
-        type: UPLOAD_FILE_FAIL,
-        payload: response.data.message
-      });
-    }
-  } catch (error) {
-    // console.log(error)
-  dispatch(setAlert('Please enter only a supported file type and a maximum of 20MB file size', 'danger'));
+  export const uploadFile = file => async dispatch => {
+    fd.append('file', file);
 
-  //   dispatch({
-  //     type: UPLOAD_FILE_FAIL,
-  //     payload: error.toString()
-  //   });
-  }
-};
-export const sendEmail = (name, to, message, link) => async dispatch => {
-  const body = JSON.stringify({
-    name,
-    to,
-    message,
-    link
-  });
-  const config = {
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
+    dispatch({
+      type: LOADING
+    });
+    const config = {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'multipart/form-data'
+      }
+    };
+    try {
+      const response = await axios.post(
+        base_url + '/api/auth/upload',
+        fd,
+        config
+      );
+      if (response.data.success) {
+        dispatch({
+          type: UPLOAD_FILE_SUCCESS,
+          payload: response.data
+        });
+      } else {
+        dispatch(setAlert('Error uploading file', 'danger'));
+        dispatch({
+          type: UPLOAD_FILE_FAIL,
+          payload: response.data.message
+        });
+      }
+    } catch (error) {
+      dispatch(setAlert('Error uploading file', 'danger'));
     }
   };
-  try {
-    const response = await axios.post(
-      base_url + '/api/auth/sendEmail',
-      body,
-      config
-    );
-    console.log(response);
-    if (response.data.success) {
-      dispatch({
-        type: SEND_EMAIL_SUCCESS
-      });
-      // dispatch(setAlert(`The file was sent to ${to} successfully`, 'success'));
-    } else {
-      // console.log(response)
-      // dispatch(setAlert('Error sending Email', 'danger'));
+  export const sendEmail = (name, to, message, link) => async dispatch => {
+    const body = JSON.stringify({
+      name,
+      to,
+      message,
+      link
+    });
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+    };
+    try {
+      const response = await axios.post(
+        base_url + '/api/auth/sendEmail',
+        body,
+        config
+      );
+      if (response.data.success) {
+        dispatch({
+          type: SEND_EMAIL_SUCCESS
+        });
+        // dispatch(setAlert(`The file was sent to ${to} successfully`, 'success'));
+      } else {
+        dispatch({
+          type: SEND_EMAIL_FAIL
+        });
+      }
+    } catch (error) {
       dispatch({
         type: SEND_EMAIL_FAIL
       });
     }
-  } catch (error) {
-    // dispatch(setAlert('Error sending Email', 'danger'));
-    dispatch({
-      type: SEND_EMAIL_FAIL
-    });
-  }
-};
-export const downloadLink = shortCode => async dispatch => {
-  try {
-    const response = await axios.post(base_url + `/${shortCode}`);
-    console.log(response);
-    if (response.data.success) {
-      dispatch({
-        type: DOWNLOAD_LINK_SUCCESS
-      });
-      dispatch(setAlert(`File downloaded successfully`, 'success'));
-    } else {
+  };
+  export const downloadLink = shortCode => async dispatch => {
+    try {
+      const response = await axios.post(base_url + `/${shortCode}`);
+      if (response.data.success) {
+        dispatch({
+          type: DOWNLOAD_LINK_SUCCESS
+        });
+        dispatch(setAlert(`File downloaded successfully`, 'success'));
+      } else {
+        dispatch(setAlert('Error downloading file', 'danger'));
+        dispatch({
+          type: DOWNLOAD_LINK_FAIL
+        });
+      }
+    } catch (error) {
       dispatch(setAlert('Error downloading file', 'danger'));
       dispatch({
         type: DOWNLOAD_LINK_FAIL
       });
     }
-  } catch (error) {
-    dispatch(setAlert('Error downloading file', 'danger'));
-    dispatch({
-      type: DOWNLOAD_LINK_FAIL
-    });
-  }
+  };
 };
