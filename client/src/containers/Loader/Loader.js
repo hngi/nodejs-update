90d176758ac4f0c181daacaee29db8a8fe89edf2
cavdy <cavdyofficials@gmail.com>
2019-10-23 17:59:50 +0100

    fix changes

--- client/src/containers/Loader/Loader.js
+++ client/src/containers/Loader/Loader.js
@@ -5,7 +5,7 @@ import './Loader.css';
 export default function loader() {
   return (
     <div className="d-flex justify-centent-center align-items-center loader">
-      <Loader type="Circles" color="#2680eb" height={100} width={100} />
+      <Loader type="Circles" color="#2680eb" height={70} width={70} />
     </div>
   );
 }
