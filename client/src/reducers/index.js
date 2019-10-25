68936bbc9877351ef7f9e147947a69fd7ba296ee
Vectormike <victorjonah199@gmail.com>
2019-10-25 17:51:04 +0100

    loading bar reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,14 @@
-import { combineReducers } from 'redux';
-import auth from './auth';
-import alert from './alert';
-import upload from './upload';
+import { combineReducers } from "redux";
+import { loadingBarReducer } from "react-redux-loading-bar";
+import auth from "./auth";
+import alert from "./alert";
+import upload from "./upload";
 
-const rootReducer = combineReducers({ auth,alert,upload });
+const rootReducer = combineReducers({
+  auth,
+  alert,
+  upload,
+  loadingBar: loadingBarReducer
+});
 
 export default rootReducer;
