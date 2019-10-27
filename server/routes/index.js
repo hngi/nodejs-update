633ff50e37766d68c735216385f945573341fd97
Vectormike <victorjonah199@gmail.com>
2019-10-27 14:17:46 +0100

    Changed routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,31 +1,31 @@
-const express = require('express');
+const express = require("express");
 const {
   redirectShortenUrl,
   downloadShortenUrl
-} = require('../controller/shortUrl');
-const { findShortenUrl } = require('../middleware/findShortenUrl');
+} = require("../controller/shortUrl");
+const { findShortenUrl } = require("../middleware/findShortenUrl");
 const router = express.Router();
-const sendEmail=require('../middleware/sendEmail')
-const loginUser = require('../controller/login');
-const registerUser = require('../controller/register');
-const uploadFile = require('../controller/upload');
-const multer = require('../middleware/multer');
-const shortenLink = require('../controller/shortUrl');
+const sendEmail = require("../middleware/sendEmail");
+const loginUser = require("../controller/login");
+const registerUser = require("../controller/register");
+const uploadFile = require("../controller/upload");
+const multer = require("../middleware/uploads");
+const shortenLink = require("../controller/shortUrl");
 
-router.get('/:shortenId', findShortenUrl,(req, res) => {
-  const fullLink = req.protocol + '://' + req.get('host');
+router.get("/:shortenId", findShortenUrl, (req, res) => {
+  const fullLink = req.protocol + "://" + req.get("host");
   const shortenId = req.params.shortenId;
-  res.render('download', { shortenId: shortenId, fullLink: fullLink})
-})
+  res.render("download", { shortenId: shortenId, fullLink: fullLink });
+});
 //router.get('/:shortenId', findShortenUrl, redirectShortenUrl);
-router.post('/api/auth/login', loginUser);
-router.post('/api/auth/register', registerUser);
-router.post('/api/auth/sendEmail', sendEmail);
-router.post('/:shortenId',findShortenUrl,downloadShortenUrl)
+router.post("/api/auth/login", loginUser);
+router.post("/api/auth/register", registerUser);
+router.post("/api/auth/sendEmail", sendEmail);
+router.post("/:shortenId", findShortenUrl, downloadShortenUrl);
 router.post(
-  '/api/auth/upload',
-  multer.multerUploads,
-  uploadFile,
-  shortenLink.shortenUrl,
+  "/api/auth/upload",
+  multer.fileUpload,
+  uploadFile.upload,
+  shortenLink.shortenUrl
 );
 module.exports = router;
