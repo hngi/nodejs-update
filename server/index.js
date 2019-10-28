0f884c59788aef5cd7cfb3b5ddae0434c6596400
Vectormike <victorjonah199@gmail.com>
2019-10-28 16:15:23 +0100

    ...

--- server/index.js
+++ server/index.js
@@ -1,21 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const expressValidator = require('express-validator');
-const mongoose = require('mongoose');
-require('./database/db');
-const router = require('./routes');
+const cors = require("cors");
+const expressValidator = require("express-validator");
+const mongoose = require("mongoose");
+require("./database/db");
+const router = require("./routes");
 // cloudinary import
-const { urlencoded, json } = require('body-parser');
-const { resolve } = require('path');
-const { uploader, cloudinaryConfig } = require('./config/cloudinary');
-const { multerUploads } = require('./middleware/multer');
-app.use('*', cloudinaryConfig);
-app.get('/', (req, res) => {
-  res.send('Connected');
+const { urlencoded, json } = require("body-parser");
+const { resolve } = require("path");
+//const { uploader, s3Config } = require("./config/aws3");
+//const { multerUploads } = require("./middleware/multer");
+//app.use("*", s3Config);
+require("dotenv").config();
+
+app.get("/", (req, res) => {
+  res.send("Connected");
 });
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public')); // using custom css and js files
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public")); // using custom css and js files
 app.use(cors());
 app.use(expressValidator());
 app.use(
@@ -25,13 +27,12 @@ app.use(
 );
 app.use(router);
 
-mongoose.set('useCreateIndex', true);
-mongoose.set('useFindAndModify', false);
-const port = process.env.PORT || 3500;
+mongoose.set("useCreateIndex", true);
+mongoose.set("useFindAndModify", false);
+const port = 3500;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-
 
 module.exports = app;
