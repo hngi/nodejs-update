18349fc37077a7441e259544b82771c1c7e6bfa8
codedcoderr <codedcoderrr@gmail.com>
2019-10-30 08:56:43 +0100

    ...

--- server/controller/shortUrl.js
+++ server/controller/shortUrl.js
@@ -19,7 +19,7 @@ const ShortenLink = {
           shortUrlParam,
           fileName,
           // shortUrl: `https://x-shareserver.herokuapp.com/${shortUrlParam}`
-          shortUrl: `http://xshare.ga/${shortUrlParam}`
+          shortUrl: `http://xshare.gq/${shortUrlParam}`
           //shortUrl: `http://localhost:4000/${shortUrlParam}`
         });
         createShortUrl.save();
