b7607720fa84c308737447e458c05414941494f3
unknown <fegoworks@hotmail.com>
2019-10-30 12:15:21 +0100

    set cookie session to never expire

--- server/middleware/validator/validateCookie.js
+++ server/middleware/validator/validateCookie.js
@@ -1,11 +1,11 @@
 const nanoid = require('nanoid');
-export const validateCookie = (req, res, next) => {
+const validateCookie = (req, res, next) => {
   if (req.cookies.userId) {
     next();
   } else {
     const id = nanoid();
     res.cookie("userId", id, {
-      maxAge: 1000 * 60 * 60 * 60 * 30,
+      maxAge: 60 * 60 * 24 * 365 * 10,
     });
     next();
   }
