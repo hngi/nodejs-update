02423c7d16f614b068ac41f643af94c363c9c29c
Vectormike <victorjonah199@gmail.com>
2019-10-31 09:51:45 +0100

    Only route for Google analytics was added here.

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,14 +3,13 @@ const {
   redirectShortenUrl,
   downloadShortenUrl
 } = require("../controller/shortUrl");
-const {
-  findShortenUrl
-} = require("../middleware/findShortenUrl");
+const { findShortenUrl } = require("../middleware/findShortenUrl");
 const router = express.Router();
 
 const sendEmail = require("../middleware/sendEmail").sendEmail;
-const saveEmail = require('../middleware/saveEmail').saveEmail;
-const sendEmailValidator = require('../middleware/validator/emailMsgValidator').validateEmail;
+const saveEmail = require("../middleware/saveEmail").saveEmail;
+const sendEmailValidator = require("../middleware/validator/emailMsgValidator")
+  .validateEmail;
 const loginUser = require("../controller/login");
 const registerUser = require("../controller/register");
 const uploadFile = require("../controller/upload");
@@ -21,8 +20,10 @@ const {
   uploadFileToS3
 } = require("../middleware/multer");
 const shortenLink = require("../controller/shortUrl");
-const validateCookie = require('../middleware/validator/validateCookie')
+const validateCookie = require("../middleware/validator/validateCookie");
+const { gaGraph, gaManual } = require("../middleware/goanalytics");
 
+// Routes
 router.get("/:shortenId", findShortenUrl, (req, res) => {
   const fullLink = req.protocol + "://" + req.get("host");
   const shortenId = req.params.shortenId;
@@ -35,11 +36,32 @@ router.get("/:shortenId", findShortenUrl, (req, res) => {
 router.post("/api/auth/login", loginUser);
 router.get("/api/auth/all", shortenLink.findAll);
 router.post("/api/auth/register", registerUser);
-router.post('/api/auth/sendEmail', sendEmailValidator, saveEmail, sendEmail);
+router.post("/api/auth/sendEmail", sendEmailValidator, saveEmail, sendEmail);
 router.post("/:shortenId", findShortenUrl, redirectShortenUrl);
-router.post("/api/auth/upload/", validateCookie, multerUploads, uploadFile, shortenLink.shortenUrl);
-router.post("/api/auth/upload/folder", upload, zipper, uploadFileToS3, shortenLink.folderUrl);
-router.get('/api/auth/upload/', (req, res) => {
-  res.render('test')
-})
+router.post(
+  "/api/auth/upload/",
+  validateCookie,
+  multerUploads,
+  uploadFile,
+  shortenLink.shortenUrl
+);
+router.post(
+  "/api/auth/upload/folder",
+  upload,
+  zipper,
+  uploadFileToS3,
+  shortenLink.folderUrl
+);
+router.get("/api/auth/upload/", (req, res) => {
+  res.render("test");
+});
+
+// Google Analytics
+
+// For test
+//router.get("/api/auth/ga", gaManual);
+
+// To display as Graph => A query is required
+//router.get("/api/auth/ga", gaGraph);
+
 module.exports = router;
