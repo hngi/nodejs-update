84a1729efb7921832497cb6633284ee11cd81628
unknown <fegoworks@hotmail.com>
2019-11-03 09:27:31 +0100

    implement changes to login page in react

--- client/src/containers/Login/Login.js
+++ client/src/containers/Login/Login.js
@@ -15,68 +15,49 @@ const Login = ({ login, history ,loading}) => {
   };
   return (
     <div>
-      <div className='login-container'>
-        <form
-          onSubmit={e => {
-            e.preventDefault();
-            login(email, password, history);
-          }}>
-          <div>
-            <h1>Login to your account</h1>
-          </div>
-          <div className='input-group'>
-            <span className='input-group-addon'>
-              <i className='fa fa-envelope fa_custom }' />
-            </span>
-            <input
-              id='email'
-              // className='form-control'
-              onChange={e => onChange(e)}
-              value={email}
-              type='email'
-              placeholder='Email Address'
-              name='email'
-              required
-            />
-          </div>
-          <div className='input-group'>
-            <span className='input-group-addon'>
-              <i className='fa fa-lock fa_custom }' />
-            </span>
-            <input
-              id='password'
-              onChange={e => onChange(e)}
-              value={password}
-              type='password'
-              placeholder='Password'
-              name='password'
-              required
-            />
-          </div>
-          <div className='r-container'>
-            {/* <label className='container'>
-              Remember me
-              <input type='checkbox' />
-              <span className='checkmark' />
-            </label>
-            <p>
-              <a href='#'>Forgot Password?</a>
-            </p> */}
-          </div>
-          <div>
-            {!loading ? (
-              <button className='upload-btn mt-4'>Login</button>
-            ) : (
-              <EmailLoader />
-            )}
-          </div>
-        </form>
-      </div>
-      <div className='signup'>
+
+      <div class="reg-container">
+    		<h2>Login</h2>
+    		<form onSubmit={e => {
+          e.preventDefault();
+          login(email, password, history);
+        }}>
+
+    			<label for="email"></label>
+    			<input id='email'
+          onChange={e => onChange(e)}
+          value={email}
+          type='email'
+          name='email'
+          required
+          placeholder="&#xf0e0;  email"
+          style="font-family:Arial, FontAwesome"/>
+
+    			<label for="password"></label>
+    			<input
+          id='password'
+          onChange={e => onChange(e)}
+          value={password}
+          type='password'
+          name='password'
+          required
+          placeholder="&#xf023;  *******"
+          style="font-family:Arial, FontAwesome">
+
+          {!loading ? (
+            <button className='btn'>Login</button>
+          ) : (
+            <EmailLoader />
+          )}
+
+    		</form>
+
         <p>
           Don't have an account? <Link to='/register'>Sign Up</Link>{' '}
         </p>
-      </div>
+
+    	</div>
+
     </div>
   );
 };
