5de82700bab3b2e260d884943f5c5f235c50a256
codedcoderr <codedcoderrr@gmail.com>
2019-11-03 12:45:57 +0100

    ...

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -11,7 +11,7 @@ import {
 const initialState = {
   token: localStorage.getItem('token'),
   loading: false,
-  user: null,
+  user: {},
   isSignedInWithGoogle: false
 };
 export default function(state = initialState, action) {
