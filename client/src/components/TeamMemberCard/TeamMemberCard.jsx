78ae6a3fad830077136f6f20cd167804d4b8a414
emex4gman <emex4gman@gmail.com>
2019-11-04 12:51:51 +0100

    update

--- client/src/components/TeamMemberCard/TeamMemberCard.jsx
+++ client/src/components/TeamMemberCard/TeamMemberCard.jsx
@@ -15,7 +15,8 @@ export default function index() {
             <div className="ml-4">
               <p className="team-member-name">{member.name}</p>
               <p className="team-member-role mt-2">{member.role}</p>
-              <a href={`${member.github}`} className="team-member-email"><div className="gitHub"></div></a>
+              <a href={`${member.github}`} className="team-member-email"><i class="fab fa-github-alt"></i></a>
+              <a href={`${member.email}`} className="team-member-email"><i class="fas fa-at"></i></a>
             </div>
           </div>
         );
