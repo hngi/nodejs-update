d0e2e1c41ab3968fde09cc13bc3ae2d9bcca6437
emex4gman <emex4gman@gmail.com>
2019-11-06 10:58:43 +0100

    preview part 1

--- client/src/containers/Upload/fileUpload.js
+++ client/src/containers/Upload/fileUpload.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import uuid from 'uuid/v4';
+import { get } from 'http';
 
 export default function fileUpload({
   file,
@@ -53,12 +54,28 @@ export default function fileUpload({
               <>
                 {file.map(i => {
                   const id = uuid();
+                  const getSize = (arr) => {
+                    if (arr <= 1000) {
+                      return `Size: ${arr }byte`
+                    }
+                    if (arr>=1000 && arr <= 100000) {
+                      return `Size: ${(arr / 1000).toFixed(1)} kb`
+                    }
+                    if (arr >= 1000000 && arr <= 100000000) {
+                      return `Size: ${(arr / 1000000).toFixed(1)}mb`
+                    }
+                    if (arr >= 1000000000) {
+                      return `Size: ${(arr / 1000000000).toFixed(1)}gb`
+                    }
+                  }
                   return (
                     <span className="uploading-file mt-3" key={id}>
                       <span className="upload-file-title">{`${i.name.substring(
                         0,
                         28
-                      )}`}</span>{' '}
+                      )}`}
+                        <br /><span className="preview">{getSize(i.size)}</span>
+                      </span>{' '}
                       <img
                         src="https://res.cloudinary.com/cavdy/image/upload/v1572357426/Group_1_gnjyx3.png"
                         alt=""
--- client/src/containers/Upload/folderUpload.js
+++ client/src/containers/Upload/folderUpload.js
@@ -54,17 +54,35 @@ export default function FolderUpload({
             {file !== '' ? (
               <>
                 {file.map(i => {
+                  const id = uuid();
+                  const getSize = (arr) => {
+                    if (arr <= 1000) {
+                      return `Size: ${arr}byte`
+                    }
+                    if (arr >= 1000 && arr <= 100000) {
+                      return `Size: ${(arr / 1000).toFixed(1)} kb`
+                    }
+                    if (arr >= 1000000 && arr <= 100000000) {
+                      return `Size: ${(arr / 1000000).toFixed(1)}mb`
+                    }
+                    if (arr >= 1000000000) {
+                      return `Size: ${(arr / 1000000000).toFixed(1)}gb`
+                    }
+                  }
+
                   return (
-                    <span className='uploading-file mt-3' key={uuid()}>
+                    <span className='uploading-file mt-3' key={id}>
                       <span className='upload-file-title'>{`${i.name.substring(
                         0,
                         28
-                      )}`}</span>{' '}
+                      )}`}
+                        <br /><span className="preview">{getSize(i.size)}</span>
+                      </span>{' '}
                       <img
                         src='https://res.cloudinary.com/cavdy/image/upload/v1572357426/Group_1_gnjyx3.png'
                         alt=''
                         className='cancel-upload'
-                        onClick={removeFile}
+                        onClick={e => removeFile(e, id, i.name)}
                       />
                     </span>
                   );
