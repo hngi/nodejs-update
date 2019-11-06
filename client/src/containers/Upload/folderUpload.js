0b8dabfa53258c47a83f4ebd5cfb2cbe01c93790
Vectormike <victorjonah199@gmail.com>
2019-11-06 12:31:03 +0100

    ...

--- client/src/containers/Upload/folderUpload.js
+++ client/src/containers/Upload/folderUpload.js
@@ -56,17 +56,33 @@ export default function FolderUpload({
             {file !== "" ? (
               <>
                 {file.map(i => {
+                  const id = uuid();
+                  const getSize = arr => {
+                    if (arr <= 1000) {
+                      return `Size: ${arr}byte`;
+                    }
+                    if (arr >= 1000 && arr <= 100000) {
+                      return `Size: ${(arr / 1000).toFixed(1)} kb`;
+                    }
+                    if (arr >= 1000000 && arr <= 100000000) {
+                      return `Size: ${(arr / 1000000).toFixed(1)}mb`;
+                    }
+                    if (arr >= 1000000000) {
+                      return `Size: ${(arr / 1000000000).toFixed(1)}gb`;
+                    }
+                  };
                   return (
                     <span className="uploading-file mt-3" key={uuid()}>
-                      <span className="upload-file-title">{`${i.name.substring(
-                        0,
-                        28
-                      )}`}</span>{" "}
+                      <span className="upload-file-title">
+                        {`${i.name.substring(0, 28)}`} <br />
+                        <span className="preview">{getSize(i.size)}</span>
+                      </span>{" "}
                       <img
                         src="https://res.cloudinary.com/cavdy/image/upload/v1572357426/Group_1_gnjyx3.png"
                         alt=""
                         className="cancel-upload"
-                        onClick={removeFile}
+                        className="cancel-upload"
+                        onClick={e => removeFile(e, id, i.name)}
                       />
                     </span>
                   );
