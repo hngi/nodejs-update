5bf45bde5a3d4713ee143908cbd5ec4b2352b222
codedcoderr <codedcoderrr@gmail.com>
2019-11-08 07:50:44 +0100

    minify

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,17 +1,7 @@
-import { SET_ALERT, REMOVE_ALERT } from "./types";
-import uuid from "uuid";
+import { SET_ALERT, REMOVE_ALERT } from './types';
+import uuid from 'uuid';
 export const setAlert = (msg, alertType, timeOut = 5000) => dispatch => {
   const id = uuid.v4();
-  dispatch({
-    type: SET_ALERT,
-    payload: { msg, alertType, id }
-  });
-  setTimeout(
-    () =>
-      dispatch({
-        type: REMOVE_ALERT,
-        payload: id
-      }),
-    timeOut
-  );
+  dispatch({ type: SET_ALERT, payload: { msg, alertType, id } });
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeOut);
 };
--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -9,19 +9,11 @@ import {
   NOT_LOADING
 } from './types';
 import { setAlert } from './alert';
-
 import axios from 'axios';
-// const base_url = 'http://localhost:4000';
 const base_url = 'http://xshare.gq';
-// const base_url = 'https://x-shareserver.herokuapp.com';
 export const login = (email, password, history) => async dispatch => {
-  const body = JSON.stringify({
-    email,
-    password
-  });
-  dispatch({
-    type: LOADING
-  });
+  const body = JSON.stringify({ email, password });
+  dispatch({ type: LOADING });
   const config = {
     headers: {
       'Content-Type': 'application/json',
@@ -35,47 +27,27 @@ export const login = (email, password, history) => async dispatch => {
       config
     );
     if (response.data.success) {
-      dispatch({
-        type: LOGIN_SUCCESS,
-        payload: response.data
-      });
-
+      dispatch({ type: LOGIN_SUCCESS, payload: response.data });
       dispatch(setAlert('Login was successful', 'success'));
       history.push('/dashboard');
     } else {
       dispatch(setAlert(response.data.message, 'danger'));
-      dispatch({
-        type: LOGIN_FAIL,
-        payload: response.data.message
-      });
+      dispatch({ type: LOGIN_FAIL, payload: response.data.message });
     }
   } catch (error) {
     dispatch(setAlert(error.toString(), 'danger'));
-
-    dispatch({
-      type: LOGIN_FAIL,
-      payload: error.toString()
-    });
+    dispatch({ type: LOGIN_FAIL, payload: error.toString() });
   }
 };
-
 export const register = (
   username,
   email,
   password,
   history
 ) => async dispatch => {
-  const body = JSON.stringify({
-    username,
-    email,
-    password
-  });
-  dispatch({
-    type: LOADING
-  });
-  const config = {
-    headers: { 'Content-Type': 'application/json' }
-  };
+  const body = JSON.stringify({ username, email, password });
+  dispatch({ type: LOADING });
+  const config = { headers: { 'Content-Type': 'application/json' } };
   try {
     const response = await axios.post(
       base_url + '/api/auth/register',
@@ -83,40 +55,23 @@ export const register = (
       config
     );
     if (response.data.success) {
-      dispatch({
-        type: REGISTER_SUCCESS,
-        payload: response.data
-      });
+      dispatch({ type: REGISTER_SUCCESS, payload: response.data });
       dispatch(setAlert('Registration was successful', 'success'));
       history.push('/login');
     } else {
       dispatch(setAlert(response.data.message, 'danger'));
-      dispatch({
-        type: REGISTER_FAIL,
-        payload: response.data.message
-      });
+      dispatch({ type: REGISTER_FAIL, payload: response.data.message });
     }
   } catch (error) {
     dispatch(setAlert(error.toString(), 'danger'));
-
-    dispatch({
-      type: REGISTER_FAIL,
-      payload: error.toString()
-    });
+    dispatch({ type: REGISTER_FAIL, payload: error.toString() });
   }
 };
-
 export const logout = () => async dispatch => {
-  dispatch({
-    type: CLEAR_PROFILE
-  });
-  dispatch({
-    type: LOGOUT
-  });
+  dispatch({ type: CLEAR_PROFILE });
+  dispatch({ type: LOGOUT });
   dispatch(setAlert('Logout was successful', 'success'));
 };
 export const notLoading = () => async dispatch => {
-  dispatch({
-    type: NOT_LOADING
-  });
+  dispatch({ type: NOT_LOADING });
 };
