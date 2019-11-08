b1f915747c99b4c5121ffac714addb499aaf1cbb
codedCoder <olowubusolaolutayo1992@gmail.com>
2019-11-08 16:18:02 +0100

    Revert "Fix build error"

--- client/src/components/TermsOfService/TermsOfService.js
+++ client/src/components/TermsOfService/TermsOfService.js
@@ -4,8 +4,7 @@ const TermsOfService = () => {
   return (
     <div>
       <section id='s1-terms'>
-        <div className='t-flex mt-5'>
-
+        <div className='t-flex'>
           <article className='terms-left term'>
             <h2>Terms of Service</h2>
             <h3>Summary</h3>
