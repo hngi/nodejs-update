2aae1d0eb5ef3ee110ff09a0c397c29b435a1751
codedcoderr <codedcoderrr@gmail.com>
2019-11-08 05:58:41 +0100

    minify code

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,2 @@
-import React from "react";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store";
-import Alert from "./components/Alert/Alert";
-import Routes from './components/Routes/Routes';
-import './oga.css'
-const App = () => {
-  return (
-    <Provider store={store}>
-      <Router>
-        <div className="d-flex flex-column parent">
-          <div className="oga">
-          <Alert />
-          <Switch>        
-            <Routes/>
-            </Switch>
-          </div>
-        </div>
-      </Router>
-    </Provider>
-  );
-};
-
-export default App;
+import React from "react";import{BrowserRouter as Router,Switch}from "react-router-dom";import{Provider}from "react-redux";import store from "./store";import Alert from "./components/Alert/Alert";import Routes from './components/Routes/Routes';import './oga.css'
+const App=()=>{return(<Provider store={store}><Router><div className="d-flex flex-column parent"><div className="oga"><Alert/><Switch><Routes/></Switch></div></div></Router></Provider>)};export default App
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,12 +3,5 @@ import auth from './auth';
 import alert from './alert';
 import upload from './upload';
 import progress from './progress';
-
-const rootReducer = combineReducers({
-  auth,
-  alert,
-  upload,
-  progress
-});
-
+const rootReducer = combineReducers({ auth, alert, upload, progress });
 export default rootReducer;
