cc99e883d36a0eae6c8284a5dc68a45b44249ccd
codedcoderr <codedcoderrr@gmail.com>
2019-11-08 06:59:17 +0100

    ...

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'jquery';
-import 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
-import * as serviceWorker from './serviceWorker';
-ReactDOM.render(<App />, document.getElementById('root')); // If you want your app to work offline and load faster,you can change // unregister() to register() below. Note this comes with some pitfalls. // Learn more about service workers:https://bit.ly/CRA-PWA serviceWorker.unregister();
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "jquery";
+import "popper.js";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
