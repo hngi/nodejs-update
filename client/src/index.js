2bbf52dd3d1ff4c92036ca91eac426573e639cc5
Vectormike <victorjonah199@gmail.com>
2019-11-09 13:08:53 +0100

    Added polyfill and browserlist

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'jquery';
-import 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
-import * as serviceWorker from './serviceWorker';
+import "react-app-polyfill/ie9";
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "jquery";
+import "popper.js";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
