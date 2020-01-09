21d1f99e7b5457ddd9ef0d3537762754204eb9a2
Vectormike <victorjonah199@gmail.com>
2020-01-09 12:55:56 +0100

    Temporary

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Footer.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css";
 export default function index() {
   const date = new Date().getFullYear();
   return (
-    <footer className='d-flex justify-content-center align-items-center'>
-      <span id='year'>
-        {date} Built by HNG 6.0 Interns |{'  '}
-        <Link to='/privacy'>Privacy </Link> |{'  '}
-        <Link to='/terms'>Terms</Link>
+    <footer className="d-flex justify-content-center align-items-center">
+      <span id="year">
+        {date} Built by Victor Jonah |{"  "}
+        <Link to="/privacy">Privacy </Link> |{"  "}
+        <Link to="/terms">Terms</Link>
       </span>
     </footer>
   );
