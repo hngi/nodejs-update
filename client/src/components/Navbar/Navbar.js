5655723eaa2215e3569d1ea2daa265dc0266e0e5
Vectormike <victorjonah199@gmail.com>
2020-01-09 12:35:05 +0100

    Temporary branch

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
-
-export default function index() {
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import "./Navbar.css";
+import { logout } from "../../actions/auth";
+const Navbar = ({ logout, isAuthenticated }) => {
   return (
     <nav className="navbar nav navbar-expand-lg navbar-light bg-light wrapper pt-3 pb-3">
       <Link className="nav__nav-logo" to="/">
@@ -19,16 +20,72 @@ export default function index() {
       >
         <span className="navbar-toggler-icon"></span>
       </button>
-
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <Link className="nav-link" to="/team">
+          {/* <li
+            className='nav-item'
+            data-toggle='collapse'
+            data-target='#navbarSupportedContent'
+            aria-controls='navbarSupportedContent'>
+            <Link className='nav-link' to='/team'>
               Meet the Team
             </Link>
-          </li>
+          </li> */}
+          {isAuthenticated ? (
+            <Fragment>
+              <li
+                className="nav-item"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+              >
+                <Link className="nav-link" to="/dashboard">
+                  <span className="hide-sm">Dashboard</span>{" "}
+                </Link>{" "}
+              </li>
+              <li
+                onClick={logout}
+                className="nav-item"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+              >
+                <Link className="nav-link" to="/login">
+                  <span className="hide-sm">Logout</span>{" "}
+                  <i className="fas fa-sign-out-alt"></i>
+                </Link>{" "}
+              </li>
+            </Fragment>
+          ) : (
+            <Fragment>
+              <li
+                className="nav-item"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+              >
+                <Link className="nav-link" to="/register">
+                  Register
+                </Link>
+              </li>
+              <li
+                className="nav-item"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+              >
+                <Link className="nav-link" to="/login">
+                  Login
+                </Link>
+              </li>
+            </Fragment>
+          )}
         </ul>
       </div>
     </nav>
   );
-}
+};
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+export default connect(mapStateToProps, { logout })(Navbar);
